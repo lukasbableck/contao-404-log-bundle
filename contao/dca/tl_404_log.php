7d9f6147842161ff446309dbeb824b3b34877601
Lukas Bableck <lukas@bableck.dev>
2025-01-18 22:42:31 +0100

    fix list, add missing translation

--- contao/dca/tl_404_log.php
+++ contao/dca/tl_404_log.php
@@ -1,10 +1,14 @@
 <?php
 
+use Contao\DataContainer;
 use Contao\DC_Table;
 
 $GLOBALS['TL_DCA']['tl_404_log'] = [
 	'config' => [
 		'dataContainer' => DC_Table::class,
+		'notEditable' => true,
+		'notCopyable' => true,
+		'closed' => true,
 		'sql' => [
 			'keys' => [
 				'id' => 'primary',
@@ -16,32 +20,35 @@ $GLOBALS['TL_DCA']['tl_404_log'] = [
 	],
 	'list' => [
 		'sorting' => [
-			'mode' => 2,
+			'mode' => DataContainer::MODE_SORTED,
 			'fields' => ['tstamp'],
-			'flag' => 1,
+			'flag' => DataContainer::SORT_ASC,
 			'panelLayout' => 'filter;search,limit',
 		],
 		'label' => [
 			'fields' => ['tstamp', 'url'],
+			'showColumns' => true,
 		],
 		'global_operations' => [
 			'all',
-			'delete'
 		],
 		'operations' => [
 			'delete',
-			'show'
+			'show',
 		],
 	],
 	'fields' => [
 		'id' => [
-			'sql' => "int(10) unsigned NOT NULL auto_increment",
+			'sql' => 'int(10) unsigned NOT NULL auto_increment',
 		],
 		'tstamp' => [
+			'flag' => DataContainer::SORT_DAY_DESC,
+			'eval' => ['rgxp' => 'datim'],
 			'sql' => "int(10) unsigned NOT NULL default '0'",
 		],
 		'rootPage' => [
 			'search' => true,
+			'foreignKey' => 'tl_page.title',
 			'sql' => "int(10) unsigned NOT NULL default '0'",
 		],
 		'ip' => [
@@ -50,15 +57,15 @@ $GLOBALS['TL_DCA']['tl_404_log'] = [
 		],
 		'url' => [
 			'search' => true,
-			'sql' => "text NULL",
+			'sql' => 'text NULL',
 		],
 		'referrer' => [
 			'search' => true,
-			'sql' => "text NULL",
+			'sql' => 'text NULL',
 		],
 		'agent' => [
 			'search' => true,
-			'sql' => "text NULL",
+			'sql' => 'text NULL',
 		],
 	],
 ];
--- contao/languages/en/default.php
+++ contao/languages/en/default.php
@@ -1,1 +1,3 @@
 <?php
+
+$GLOBALS['TL_LANG']['MOD']['tl_404_log'] = ['404 Log', ''];
